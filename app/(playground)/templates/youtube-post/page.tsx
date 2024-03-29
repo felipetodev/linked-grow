2c5d79b253344b11e13e5a3ef78e2ef38b4bdbd8
felipetodev <fe.ossandon.u@gmail.com>
2024-03-29 20:57:17 -0300

    feat: add mock yt feature

---
+++ app/(playground)/templates/youtube-post/page.tsx
@@ -0,0 +1,7 @@
+export default function YouTubePostPage() {
+  return (
+    <div>
+      <h1>YouTubePostPage</h1>
+    </div>
+  )
+}
--- components/features-cards.tsx
+++ components/features-cards.tsx
@@ -1,17 +1,36 @@
 import Link from "next/link";
 import { Separator } from "./ui/separator";
 
+const randomId = crypto.randomUUID()
+
+const FEATURES_CARDS = [
+  {
+    href: `/templates/${randomId}`,
+    title: "Genera un post con IA desde una plantilla",
+    description: "Usa el poder de la inteligencia artificial para generar contenido de calidad en tus post de LinkedIn.",
+  },
+  {
+    href: '/templates/youtube-post',
+    title: "Genera un post con IA desde un video de YouTube (PRONTO)",
+    description: "Comparte tus videos de YouTube en LinkedIn con un post generado por inteligencia artificial.",
+  },
+]
+
 export function FeaturesCards() {
-  const randomId = crypto.randomUUID()
   return (
-    <Link href={`/templates/${randomId}`} className="hover:opacity-80 flex flex-col p-2 bg-gradient-to-r from-pink-500 to-yellow-500 h-[250px] w-[250px] rounded transition-opacity">
-      <h2 className="text-2xl font-semibold my-4">
-        Genera un post con IA desde una plantilla
-      </h2>
-      <Separator className="mb-10" />
-      <span className="text-xs">
-        Usa el poder de la inteligencia artificial para generar contenido de calidad en tus post de LinkedIn.
-      </span>
-    </Link>
+    <div className="grid gap-8 grid-cols-[repeat(auto-fill,_minmax(210px,250px))]">
+      {
+        FEATURES_CARDS.map((card) => (
+          <Link key={card.href} href={card.href} className="hover:opacity-80 flex flex-col p-2 bg-gradient-to-r from-pink-500 to-yellow-500 rounded transition-opacity">
+            <h2 className="text-2xl font-semibold my-4">
+              {card.title}
+            </h2>
+            <Separator className="mb-4" />
+            <span className="text-xs">
+              {card.description}
+            </span>
+          </Link>
+        ))}
+    </div>
   )
 }
