c1ff36e07def7964c4c832529b4248caa39f4868
felipetodev <fe.ossandon.u@gmail.com>
2024-03-28 21:45:03 -0300

    fix: remove skeleton bg hover

--- app/ideas/components/cards-skeleton.tsx
+++ app/ideas/components/cards-skeleton.tsx
@@ -4,7 +4,7 @@ export function CardsSkeleton() {
   return (
     <div className="grid sm:grid-cols-2 gap-6">
       {Array.from({ length: 6 }).map((_, i) => (
-        <div key={i} className="flex flex-col justify-between gap-y-2 h-[130px] border rounded p-2 transition hover:-translate-y-0.5 hover:bg-secondary">
+        <div key={i} className="flex flex-col justify-between gap-y-2 h-[130px] border rounded p-2 transition-transform hover:-translate-y-0.5">
           <Skeleton className="w-full h-full" />
           <div className="flex gap-x-2 w-full h-[50px]">
             <Skeleton className="w-full" />
