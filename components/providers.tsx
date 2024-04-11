b4469772bdddab910f9fe3764206174e6accfb0f
felipetodev <fe.ossandon.u@gmail.com>
2024-04-11 13:06:42 -0400

    feat: add convex provider + clerk

--- components/providers.tsx
+++ components/providers.tsx
@@ -2,15 +2,21 @@
 
 import * as React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
-import { ClerkProvider } from '@clerk/nextjs'
+import { ClerkProvider, useAuth } from "@clerk/nextjs"
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+import { ConvexReactClient } from "convex/react";
 import { type ThemeProviderProps } from "next-themes/dist/types"
+
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL as string);
 
 export function Providers({ children, ...props }: ThemeProviderProps) {
   return (
-    <ClerkProvider>
-      <NextThemesProvider {...props}>
-        {children}
-      </NextThemesProvider>
+    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
+      <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+        <NextThemesProvider {...props}>
+          {children}
+        </NextThemesProvider>
+      </ConvexProviderWithClerk>
     </ClerkProvider>
   )
 }
