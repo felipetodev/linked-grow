ebcc3d127131f7b45d949c713ddf6f9907a9319e
felipetodev <fe.ossandon.u@gmail.com>
2024-04-12 17:04:07 -0400

    fix: refact tw class

--- app/ideas/components/cards-skeleton.tsx
+++ app/ideas/components/cards-skeleton.tsx
@@ -5,7 +5,7 @@ export function CardsSkeleton() {
     <div className="grid sm:grid-cols-2 gap-6">
       {Array.from({ length: 6 }).map((_, i) => (
         <div key={i} className="flex flex-col justify-between gap-y-2 h-[130px] border rounded p-2 transition-transform hover:-translate-y-0.5">
-          <Skeleton className="w-full h-full" />
+          <Skeleton className="size-full" />
           <div className="flex gap-x-2 w-full h-[50px]">
             <Skeleton className="w-full" />
             <Skeleton className="w-full" />
