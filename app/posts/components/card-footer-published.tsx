5fe366f1db744f113ab9e385a8ab67c3e09e9bdf
felipetodev <fe.ossandon.u@gmail.com>
2024-04-12 21:01:47 -0400

    feat: allow copy a posted published as a new draft

--- app/posts/components/card-footer-published.tsx
+++ app/posts/components/card-footer-published.tsx
@@ -2,7 +2,12 @@ import { Button, buttonVariants } from "@/components/ui/button"
 import { IconCopy, IconLink } from "@tabler/icons-react"
 import { cn } from "@/lib/utils"
 
-export function CardFooterPublished({ postUrn }: { postUrn?: string }) {
+type Props = {
+  postUrn?: string
+  onCopyPost: () => void
+}
+
+export function CardFooterPublished({ postUrn, onCopyPost }: Props) {
   return (
     <footer className="flex gap-4 mt-auto">
       <a
@@ -14,7 +19,7 @@ export function CardFooterPublished({ postUrn }: { postUrn?: string }) {
         Ver post
         <IconLink size={20} className="ml-2" />
       </a>
-      <Button className="w-full" disabled>
+      <Button className="w-full" onClick={onCopyPost}>
         Copiar
         <IconCopy size={20} className="ml-2" />
       </Button>
--- app/posts/components/post-card.tsx
+++ app/posts/components/post-card.tsx
@@ -1,8 +1,14 @@
+import { useState } from "react"
+import { useRouter } from "next/navigation"
 import { MemoizedReactMarkdown } from "@/components/ui/markdown"
 import TimeAgo from "@/lib/hooks/use-time-ago"
 import { CardFooterDraft } from "./card-footer-draft"
-import { type Id } from "@/convex/_generated/dataModel"
 import { CardFooterPublished } from "./card-footer-published"
+import { IconLoader } from "@tabler/icons-react"
+import { useMutation } from "convex/react"
+import { api } from "@/convex/_generated/api"
+import { toast } from "sonner"
+import { type Id } from "@/convex/_generated/dataModel"
 
 const maxChars = 200
 
@@ -23,11 +29,23 @@ export function PostCard({
   postUrn,
   updatedAt,
 }: Props) {
+  const [isLoading, setIsLoading] = useState(false)
   const contentLength = content.length
   const content_ = contentLength > maxChars ? content.slice(0, maxChars) + '...' : content
+  const router = useRouter()
+
+  const createPost = useMutation(api.posts.createPost)
+
+  const handleCreatePostCopy = async () => {
+    setIsLoading(true)
+    const postId = await createPost({ content, status: 'draft' })
+
+    toast.success('Post copiado como borrador')
+    router.push(`/new/${postId}`)
+  }
 
   return (
-    <div className="grid border h-[400px] max-h-[400px] p-4 rounded shadow-lg overflow-hidden transition duration-300 hover:-translate-y-2 hover:shadow-xl">
+    <div className="relative grid border h-[400px] max-h-[400px] p-4 rounded shadow-lg overflow-hidden transition duration-300 hover:-translate-y-2 hover:shadow-xl">
       <div className="flex items-center mb-4">
         <div className="flex justify-center items-center size-[48px] p-4 border rounded-full bg-background ">
           {author?.split(' ').map(name => name[0]).join('') ?? 'U'}
@@ -57,7 +75,19 @@ export function PostCard({
         {content_}
       </MemoizedReactMarkdown>
       {status === 'draft' && <CardFooterDraft postId={postId} />}
-      {status === 'published' && <CardFooterPublished postUrn={postUrn} />}
+      {status === 'published' && (
+        <CardFooterPublished
+          postUrn={postUrn}
+          onCopyPost={handleCreatePostCopy}
+        />
+      )}
+      {isLoading && (
+        <div className="text-primary flex flex-col justify-center items-center absolute inset-0 size-full backdrop-blur-sm bg-background/30">
+          <h2 className="font-semibold">Copiando contenido como borrador</h2>
+          <span className="text-sm font-medium">Por favor, espere...</span>
+          <IconLoader className="animate-spin size-8 mt-4" />
+        </div>
+      )}
     </div>
   )
 }
