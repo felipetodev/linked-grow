2c32bca831525f9b180b5dda6db728c5e9ff2b09
felipetodev <fe.ossandon.u@gmail.com>
2024-04-16 22:20:35 -0400

    feat: add file upload crud for /post (db & linkedin)

--- components/posts/post-actions.tsx
+++ components/posts/post-actions.tsx
@@ -1,3 +1,4 @@
+import { usePathname } from "next/navigation";
 import { IconBookmark, IconEdit } from "@tabler/icons-react";
 import { Button } from "@/components/ui/button";
 import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip";
@@ -5,54 +6,63 @@ import { usePost } from "@/components/posts/post-context";
 import { PostEditDrawer } from "@/components/posts/post-edit-drawer";
 import { useClerk, useUser } from "@clerk/nextjs";
 import { cn } from "@/lib/utils";
-import { usePathname } from "next/navigation";
 
 export function PostActions() {
-  const { postId, isLoading, hasChanges, onSave } = usePost();
+  const { postId, isSuccess, isLoading, hasChanges, onSave } = usePost();
   const pathname = usePathname();
   const { isSignedIn } = useUser();
   const clerk = useClerk();
 
   return (
-    <footer className="flex gap-x-4 w-full">
-      <Tooltip delayDuration={0}>
-        <TooltipTrigger asChild>
-          {isSignedIn ? (
-            <Button
-              size='sm'
-              disabled={(!hasChanges && Boolean(postId))}
-              className={cn("size-9 p-0 w-full", {
-                'bg-green-600 cursor-not-allowed': (!hasChanges && postId)
-              })}
-              onClick={onSave}
-            >
-              {isLoading && <span className="mr-1.5">Guardando...</span>}
-              <IconBookmark size={20} />
-            </Button>
+    <footer className="flex gap-x-4 w-full px-4">
+      {isSuccess ? (
+        <Button
+          size='sm'
+          onClick={onSave}
+          disabled={isSuccess}
+          className='size-9 p-0 w-full bg-green-600 text-white cursor-not-allowed'
+        >
+          Post publicado ✨
+        </Button>
+      ) : (
+        <Tooltip delayDuration={0}>
+          <TooltipTrigger asChild>
+            {isSignedIn ? (
+              <Button
+                size='sm'
+                disabled={(!hasChanges && Boolean(postId))}
+                className={cn("size-9 p-0 w-full", {
+                  'bg-green-600 text-white cursor-not-allowed': (!hasChanges && postId)
+                })}
+                onClick={onSave}
+              >
+                {isLoading && <span className="mr-1.5">Guardando...</span>}
+                <IconBookmark size={20} />
+              </Button>
 
-          ) : (
-            <Button
-              size='sm'
-              className="size-9 p-0 w-full"
-              onClick={() => clerk.openSignIn({ redirectUrl: pathname })}
-            >
-              <IconBookmark size={20} />
-            </Button>
-          )}
-        </TooltipTrigger>
-        <TooltipContent className="text-xs flex items-center gap-4">
-          Guardar post
-        </TooltipContent>
-      </Tooltip>
-
+            ) : (
+              <Button
+                size='sm'
+                className="size-9 p-0 w-full"
+                onClick={() => clerk.openSignIn({ redirectUrl: pathname })}
+              >
+                <IconBookmark size={20} />
+              </Button>
+            )}
+          </TooltipTrigger>
+          <TooltipContent className="text-xs flex items-center gap-4">
+            Guardar post
+          </TooltipContent>
+        </Tooltip>
+      )}
       {isSignedIn ? (
         <Tooltip delayDuration={0}>
           <PostEditDrawer
             // postId={postId}
             // text={editText ?? text}
             disableSave={(!hasChanges && Boolean(postId))}
-            // onEditText={setEditText}
-            // onSavePost={handleSavePost}
+          // onEditText={setEditText}
+          // onSavePost={handleSavePost}
           >
             <TooltipTrigger asChild>
               <Button size='sm' className="size-9 p-0 w-full">
--- components/posts/post-context.tsx
+++ components/posts/post-context.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import * as React from "react";
+import Image from "next/image";
 import { useStreamableText } from "@/lib/hooks/use-streamable-text";
 import { MemoizedReactMarkdown } from "@/components/ui/markdown";
 import { publishPost } from "@/app/actions/post";
@@ -9,6 +10,7 @@ import { PostHeader } from "./post-header";
 import { toast } from "sonner";
 import { useMutation } from "convex/react";
 import { api } from "@/convex/_generated/api";
+import { useFileUpload } from "@/components/upload-files-context";
 import confetti from "canvas-confetti";
 import { type StreamableValue } from "ai/rsc";
 import { type Id } from "@/convex/_generated/dataModel";
@@ -49,27 +51,63 @@ const Post = React.forwardRef<
   const canvas = React.useRef<HTMLCanvasElement>(null);
   const successRef = React.useRef(false);
   const urlRef = React.useRef("");
+  const fileIdRef = React.useRef<Id<"_storage"> | undefined>()
   const text = useStreamableText(content)
+
+  const { file, onDeletePostFile } = useFileUpload();
 
   const createPost = useMutation(api.posts.createPost)
   const updatePost = useMutation(api.posts.updatePost)
+  const generateUploadUrl = useMutation(api.files.generateUploadUrl)
+
+  const getConvexStorageId = async (file: File) => {
+    const postUrl = await generateUploadUrl();
+
+    const result = await fetch(postUrl, {
+      method: "POST",
+      headers: { "Content-Type": file.type },
+      body: file,
+    });
+
+    const { storageId } = await result.json();
+    return storageId
+  }
 
   const handleSavePost = async () => {
     setIsLoading(true)
     submittedContent.current = editText
 
+    if (file && !fileIdRef.current) {
+      // upload new file
+      const fileId = await getConvexStorageId(file)
+      fileIdRef.current = fileId
+    }
+
+    if (file && fileIdRef.current && postId) {
+      // delete prev file
+      onDeletePostFile({ fileId: fileIdRef.current, postId, deletePreview: false })
+      // upload new file
+      const fileId = await getConvexStorageId(file)
+      fileIdRef.current = fileId
+    }
+
+    if (!file && fileIdRef.current && postId) {
+      // delete file and update post with no file
+      onDeletePostFile({ fileId: fileIdRef.current, postId, deletePreview: true })
+      fileIdRef.current = undefined
+    }
+
     if (submittedContent.current === editText && postId) {
-      await updatePost({ content: editText ?? text, postId, status: 'draft' })
+      await updatePost({ content: editText ?? text, postId, status: 'draft', imgFileId: fileIdRef.current })
       toast.success('Post guardado')
       setIsLoading(false)
       return
     }
 
     try {
-      const postId = await createPost({ content: editText ?? text, status: 'draft' })
+      const postId = await createPost({ content: editText ?? text, status: 'draft', imgFileId: fileIdRef.current })
       toast.success('Post guardado')
       setPostId(postId)
-      setIsLoading(false)
     } catch {
       toast.error('Error al guardar el post')
     } finally {
@@ -80,7 +118,30 @@ const Post = React.forwardRef<
   const handlePublishPost = async (comment?: string) => {
     setIsLoading(true)
 
-    const result = await publishPost({ text, comment })
+    let formData: null | FormData = null
+
+    if (file && !fileIdRef.current) {
+      const postUrl = await generateUploadUrl();
+
+      const result = await fetch(postUrl, {
+        method: "POST",
+        headers: { "Content-Type": file.type },
+        body: file,
+      });
+
+      const { storageId: fileId } = await result.json();
+      fileIdRef.current = fileId
+
+      formData = new FormData()
+      formData.append("file", file)
+    }
+
+    if (file && fileIdRef.current) {
+      formData = new FormData()
+      formData.append("file", file)
+    }
+
+    const result = await publishPost({ text: editText ?? text, comment, file: formData })
 
     if (result && 'error' in result) {
       toast.error(result.error) // use toast
@@ -90,9 +151,9 @@ const Post = React.forwardRef<
 
     if (result.postUrn) {
       if (postId) {
-        await updatePost({ postId, content: text, status: "published", postUrn: result.postUrn })
+        await updatePost({ postId, content: text, status: "published", postUrn: result.postUrn, imgFileId: fileIdRef.current })
       } else {
-        await createPost({ content: text, status: "published", postUrn: result.postUrn })
+        await createPost({ content: text, status: "published", postUrn: result.postUrn, imgFileId: fileIdRef.current })
       }
 
       const cannon = confetti.create(canvas.current as HTMLCanvasElement, {
@@ -131,15 +192,15 @@ const Post = React.forwardRef<
     >
       <div
         ref={ref}
-        className="border rounded group relative flex flex-col gap-y-8 items-start p-2"
+        className="border rounded group relative flex flex-col gap-y-8 items-start py-2"
       >
         {text?.length > 0 && (
-          <span className="absolute top-0 right-0 font-semibold text-xs opacity-60 p-2">
+          <span className="absolute top-0 right-0 font-semibold text-xs opacity-60 px-4 py-2">
             {text.length} caracteres
           </span>
         )}
         <PostHeader />
-        <div className="flex-1 space-y-2 overflow-hidden px-1">
+        <div className="flex-1 space-y-2 overflow-hidden px-4">
           <MemoizedReactMarkdown
             className="prose break-words dark:prose-invert prose-p:leading-relaxed prose-pre:p-0"
             components={{
@@ -151,6 +212,15 @@ const Post = React.forwardRef<
             {editText ?? text}
           </MemoizedReactMarkdown>
         </div>
+        {file && (
+          <Image
+            src={URL.createObjectURL(file)}
+            alt="file"
+            width={0}
+            height={500}
+            className="size-full max-h-[500px] object-contain"
+          />
+        )}
         <PostActions />
       </div>
     </PostContext.Provider>
