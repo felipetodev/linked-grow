dd5dbec948bc4a3fc7c5f21bbd8097df4763b9d3
felipetodev <fe.ossandon.u@gmail.com>
2024-04-21 22:43:17 -0400

    fix: move header to layout "new"

--- app/(dashboard)/new/[id]/page.tsx
+++ app/(dashboard)/new/[id]/page.tsx
@@ -1,5 +1,3 @@
-import { Suspense } from "react"
-import MembershipSSR from "@/components/memberships-ssr"
 import { WritePost } from "@/components/write/write-post"
 import { type Id } from "@/convex/_generated/dataModel"
 
@@ -11,19 +9,6 @@ type Props = {
 
 export default async function NewPostPage({ params: { id } }: Props) {
   return (
-    <>
-      <div className="hidden xl:grid md:grid-cols-2 bg-background px-4 py-2 rounded mb-2">
-        <div className="flex items-center border-r font-semibold">
-          Editar / Crear Post
-          <Suspense fallback={<></>}>
-            <MembershipSSR />
-          </Suspense>
-        </div>
-        <div className="ml-4 flex items-center font-semibold">
-          Post Preview
-        </div>
-      </div>
-      <WritePost postId={id} />
-    </>
+    <WritePost postId={id} />
   )
 }
--- app/(dashboard)/new/layout.tsx
+++ app/(dashboard)/new/layout.tsx
@@ -1,3 +1,5 @@
+import { Suspense } from "react"
+import MembershipSSR from "@/components/memberships-ssr"
 import { FileUploadProvider } from "@/components/upload-files-context"
 
 interface LayoutProps {
@@ -7,6 +9,17 @@ interface LayoutProps {
 export default function WriteLayout({ children }: LayoutProps) {
   return (
     <FileUploadProvider>
+      <div className="hidden xl:grid md:grid-cols-2 bg-background px-4 py-2 rounded mb-2">
+        <div className="flex items-center border-r font-semibold">
+          Editar / Crear Post
+          <Suspense fallback={<></>}>
+            <MembershipSSR />
+          </Suspense>
+        </div>
+        <div className="ml-4 flex items-center font-semibold">
+          Post Preview
+        </div>
+      </div>
       {children}
     </FileUploadProvider>
   )
