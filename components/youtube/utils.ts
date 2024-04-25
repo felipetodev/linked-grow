e65612f24f72ecedb60522fc3f4828cbc5bbe5e8
felipetodev <fe.ossandon.u@gmail.com>
2024-04-25 11:33:26 -0400

    fix: use langchain youtubeId regex

--- components/youtube/utils.ts
+++ components/youtube/utils.ts
@@ -1,16 +1,13 @@
-export function getVideoId(url: string) {
-  if (
-    !url.trim() ||
-    !(
-      url.includes("https://www.youtube.com/watch?v=") ||
-      url.includes("https://youtu.be")
-    )
-  ) return ""
+// https://github.com/langchain-ai/langchainjs/blob/a8f7f5c4bd3f09fc5e3dd63b3e81426f4472125d/langchain/src/document_loaders/web/youtube.ts#L62
 
-  const youtubeRegex = /^(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/;
-  const match = url.match(youtubeRegex);
-
-  if (!match) return "";
-
-  return match[1];
+export const getVideoId = (url: string): string => {
+  if (!url.trim()) return "";
+  const match = url.match(
+    /.*(?:youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=)([^#&?]*).*/
+  );
+  if (match !== null && match[1].length === 11) {
+    return match[1];
+  } else {
+    return ""
+  }
 }
